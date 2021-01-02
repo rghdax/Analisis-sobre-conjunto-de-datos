a73c5cf8860e4f4d7c27af5358f040fb264a6578
dbagan13 <al341841@uji.es>
2021-01-02 11:00:03 +0100

    Add files via upload

--- pra2.R
+++ pra2.R
@@ -7,12 +7,11 @@ colSums(df=="NaN")
 
 
 #Runtime
-horas <- as.integer(substr(runtime, 1, 1))    
-minutos <- as.integer(substr(runtime, 3, 4))    
+horas <- as.integer(substr(df$Runtime, 1, 1))    
+minutos <- as.integer(substr(df$Runtime, 3, 4)) 
 runtime <- 60*horas+minutos
 df$Runtime <- runtime
 summary(df$Runtime)
-
 #Fechas
 colnames(df)[10] <- "Release.Date.Theaters"
 colnames(df)[11] <- "Release.Date.Streamings"
@@ -23,35 +22,77 @@ dim(df[!df$Release.Date.Streamings=="" & df$Release.Date.Theaters=="",])
 dim(df[df$Release.Date.Streamings=="" & df$Release.Date.Theaters=="",])
 
 
-
 library(stringr)
-streamings.year <-  as.integer(str_sub(df$Release.Date.Streamings, -4, -1)) 
-streamings.month <-  substr(df$Release.Date.Streamings, 1, 3)
-streamings.day <-  as.integer(gsub(",","",substr(df$Release.Date.Streamings, 4, 5))) 
-months <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug","Sep", "Oct", "Nov", "Dec" )
-streamings.month <- match(streamings.month,months)
-streamings.year_day <- theaters.day * theaters.month
+#-----------Streamings-------------
 
-#Streamings 
+#Separacion fecha
+months <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug","Sep", "Oct", "Nov", "Dec" )
 streamings.year <-  as.integer(str_sub(df$Release.Date.Streamings, -4, -1)) 
 streamings.month <-  substr(df$Release.Date.Streamings, 1, 3)
 streamings.day <-  as.integer(gsub(",","",substr(df$Release.Date.Streamings, 4, 5))) 
 streamings.month <- match(streamings.month,months)
 streamings.year_day <- streamings.day * streamings.month
+streamings.year_day <- paste(str_sub(streamings.year, -2, -1),streamings.month,streamings.day,sep="/")
+streamings.year_day <- as.Date(streamings.year_day,format="%D")
+streamings.year_day <- as.integer(strftime(streamings.year_day, format = "%j"))
+streamings <- data.frame(year=streamings.year,month=streamings.month,
+                         day=streamings.day,year_day=streamings.year_day)
+#Estación
+spring <-  as.integer(strftime("2020-03-21", format = "%j"))
+summer <-  as.integer(strftime("2020-06-22", format = "%j"))
+autumn <-  as.integer(strftime("2020-09-23", format = "%j"))
+winter <-  as.integer(strftime("2020-12-22", format = "%j"))
+streamings$seasons[!is.na(streamings$year)] <- "winter"
+streamings$seasons[streamings$year_day>spring] <- "spring" 
+streamings$seasons[streamings$year_day>summer] <- "summer" 
+streamings$seasons[streamings$year_day>autumn] <- "autumn" 
+streamings$seasons[streamings$year_day>winter] <- "winter" 
 
-#Theaters
-theaters <- df$Release.Date.Theaters
+head(streamings)
+df$streamings.year <- streamings$year
+df$streamings.month <- streamings$month
+df$streamingss.day <- streamings$day
+df$streamings.year_day <- streamings$year_day
+df$streamings.seasons <- streamings$seasons
+df$Release.Date.Streamings <- NULL
+
+#----------------Theaters----------------
 #Wide o limited
-release_type <- theaters
+theaters <- df$Release.Date.Theaters
+release_type <- df$Release.Date.Theaters
 release_type[str_sub(release_type, -4, -1)=="wide"] <- "wide"
 release_type[str_sub(release_type, -7, -1)=="limited"] <- "limited"
+
 #Limpieza
 theaters <- str_remove_all(theaters,"limited")
 theaters <- str_remove_all(theaters,"wide")
 theaters <- str_sub(theaters, 1, -3)
-#Theaters
+head(theaters)
+
+#Separacion fecha
 theaters.year <-  as.integer(str_sub(theaters, -4, -1)) 
 theaters.month <-  substr(theaters, 1, 3)
 theaters.day <-  as.integer(gsub(",","",substr(theaters, 4, 5))) 
 theaters.month <- match(theaters.month,months)
-theaters.year_day <- theaters.day * theaters.month
+theaters.year_day <- paste(str_sub(theaters.year, -2, -1),theaters.month,theaters.day,sep="/")
+theaters.year_day <- as.Date(theaters.year_day,format="%D")
+theaters.year_day <- as.integer(strftime(theaters.year_day, format = "%j"))
+theaters <- data.frame(year=theaters.year,month=theaters.month,
+                         day=theaters.day,year_day=theaters.year_day)
+#Estación
+theaters$seasons[!is.na(theaters$year)] <- "winter"
+theaters$seasons[theaters$year_day>spring] <- "spring" 
+theaters$seasons[theaters$year_day>summer] <- "summer" 
+theaters$seasons[theaters$year_day>autumn] <- "autumn" 
+theaters$seasons[theaters$year_day>winter] <- "winter" 
+
+head(theaters)
+
+df$theaters.year <- theaters$year
+df$theaters.month <- theaters$month
+df$theaters.day <- theaters$day
+df$theaters.year_day <- theaters$year_day
+df$theaters.seasons <- theaters$seasons
+df$Release.Date.Theaters <- NULL
+
+str(df)
